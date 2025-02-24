390b921bdaf97c9a0bfcb808ec92700809c8274d
Guillaume WALTER <walterguillaume53@gmail.com>
2025-02-24 21:41:12 +0100

    added McBotApp.form

--- src/main/java/me/gamordstrimer/mcbotapp/McBotApp.java
+++ src/main/java/me/gamordstrimer/mcbotapp/McBotApp.java
@@ -1,10 +1,8 @@
 package me.gamordstrimer.mcbotapp;
 
-import me.gamordstrimer.mcbotapp.app.AppFrame;
-
 public class McBotApp {
 
     public static void main(String[] args) {
-        new AppFrame();
+        new McBotApp();
     }
 }
